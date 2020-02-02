08681aff213acdeeaa00dbc6bf0607041eaeafe9
Бернова Анна <bast33@yandex.ru>
2020-02-02 12:01:57 +0300

    add input

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,14 @@
 <template>
-  <div >
-   <h1>Create</h1>
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <h1>Create task</h1>
+      <form>
+         <div class="input-field">
+          <input  id="title" type="text" class="validate" required>
+          <label for="title">Title</label>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -9,7 +17,7 @@
 
 
 export default {
-  name: 'home',
+  name: 'create',
   components: {
    
   }
