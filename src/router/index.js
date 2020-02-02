3e34021405bb2022732e94d3a916d5cbb5cb0074
Бернова Анна <bast33@yandex.ru>
2020-02-02 11:53:34 +0300

    dynamic import for Create

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Create from '../views/Create.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Create
+    name: 'create',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Create.vue')
   },
   {
     path: '/list',
